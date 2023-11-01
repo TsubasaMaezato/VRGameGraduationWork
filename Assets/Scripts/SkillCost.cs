ccb47387016861249ea5bfe035a9ff6c91de5e85
TsubasaMaezato <c22024@std.it-college.ac.jp>
2023-11-01 16:41:32 +0900

    目が開いたり閉じたりするようになりました。

--- Assets/Scripts/SkillCost.cs
+++ Assets/Scripts/SkillCost.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class SkillCost : MonoBehaviour
 {
+    public Animator[] Anim;
+
+    int animCount;
+    int cameraAnimNum;
     public int cost;
     float costTimer;
 
     public GameObject eyeHide;
     public GameObject ghost;
+    public GameObject[] eyeCameraObj;
 
     bool mapSkillRoadDifenseRight;
     bool mapSkillRoadDifenseLeft;
@@ -27,7 +32,19 @@ public class SkillCost : MonoBehaviour
 
         rawImage.texture = rt[0];
 
+        cameraAnimNum = 0;
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
+
         sound = GetComponent<AudioSource>();
+        Anim[0] = eyeCameraObj[0].GetComponent<Animator>();
+        Anim[1] = eyeCameraObj[1].GetComponent<Animator>();
+        Anim[2] = eyeCameraObj[2].GetComponent<Animator>();
+        Anim[3] = eyeCameraObj[3].GetComponent<Animator>();
+        Anim[4] = eyeCameraObj[4].GetComponent<Animator>();
+        Anim[5] = eyeCameraObj[5].GetComponent<Animator>();
+        Anim[6] = eyeCameraObj[6].GetComponent<Animator>();
+        Anim[7] = eyeCameraObj[7].GetComponent<Animator>();
+        Anim[8] = eyeCameraObj[8].GetComponent<Animator>();
     }
     void Update()
     {
@@ -37,6 +54,8 @@ public class SkillCost : MonoBehaviour
             costTimer = 0;
             cost += 1;
         }
+
+        EyeCamera();
     }
     public void SoundRing()
     {
@@ -78,41 +97,98 @@ public class SkillCost : MonoBehaviour
 
     public void MainHall()
     {
+        cameraAnimNum = 0;
         rawImage.texture = rt[0];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void RoadDefenseRight()
     {
+        cameraAnimNum = 1;
         rawImage.texture = rt[1];
         mapSkillRoadDifenseRight = true;
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void RoadDefenseLeft()
     {
+        cameraAnimNum = 2;
         rawImage.texture = rt[2];
         mapSkillRoadDifenseLeft = true;
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
 
     public void Room1()
     {
+        cameraAnimNum = 3;
         rawImage.texture = rt[3];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void Room2()
     {
+        cameraAnimNum = 4;
         rawImage.texture = rt[4];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void Room3()
     {
+        cameraAnimNum = 5;
         rawImage.texture = rt[5];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void Room4()
     {
+        cameraAnimNum = 6;
         rawImage.texture = rt[6];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void Room5()
     {
+        cameraAnimNum = 7;
         rawImage.texture = rt[7];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
     }
     public void Room6()
     {
+        cameraAnimNum = 8;
         rawImage.texture = rt[8];
+        Anim[cameraAnimNum].SetBool("OpenBool", true);
+    }
+    void EyeCamera()
+    {
+        if (cameraAnimNum != 0)
+        {
+            Anim[0].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 1)
+        {
+            Anim[1].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 2)
+        {
+            Anim[2].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 3)
+        {
+            Anim[3].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 4)
+        {
+            Anim[4].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 5)
+        {
+            Anim[5].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 6)
+        {
+            Anim[6].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 7)
+        {
+            Anim[7].SetBool("OpenBool", false);
+        }
+        if (cameraAnimNum != 8)
+        {
+            Anim[8].SetBool("OpenBool", false);
+        }
     }
 }
